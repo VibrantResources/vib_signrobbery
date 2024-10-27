a7766e3a2ca462a57d0ecd347bb66bd3b2a6dd16
VibrantRP <127861623+VibrantRP@users.noreply.github.com>
2024-10-27 23:35:15 +0000

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,62 @@
+Config = Config or {}
+
+Config.GenericStuff = {
+    Debug = false,
+    SignCutTimeInSeconds = 3, -- How long it takes to cut down a sign
+}
+
+Config.Signs = { -- Add signs using the same below format that aren't already here
+    ['prop_sign_road_01a'] = {
+        Model = 'prop_sign_road_01a',
+        Item = 'stopsign',
+        Hash = 	-949234773,
+    },
+
+    ['prop_sign_road_05a'] = {
+        Model = 'prop_sign_road_05a',
+        Item = 'walkingmansign',
+        Hash = 1502931467,
+    },
+
+    ['prop_sign_road_03e'] = {
+        Model = 'prop_sign_road_03e',
+        Item = 'dontblockintersectionsign',
+        Hash = 1191039009,
+    },
+
+    ['prop_sign_road_03m'] = {
+        Model = 'prop_sign_road_03m',
+        Item = 'uturnsign',
+        Hash = -156356737,
+    },
+
+    ['prop_sign_road_04a'] = {
+        Model = 'prop_sign_road_04a',
+        Item = 'noparkingsign',
+        Hash = -463994753,
+    },
+
+    ['prop_sign_road_05e'] = {
+        Model = 'prop_sign_road_05e',
+        Item = 'leftturnsign',
+        Hash = -1651641860,
+    },
+
+    ['prop_sign_road_05f'] = {
+        Model = 'prop_sign_road_05f',
+        Item = 'rightturnsign',
+        Hash = 	793482617,
+    },
+
+    ['prop_sign_road_restriction_10'] = {
+        Model = 'prop_sign_road_restriction_10',
+        Item = 'notrespassingsign',
+        Hash = 1021214550,
+    },
+    
+    ['prop_sign_road_02a'] = {
+        Model = 'prop_sign_road_02a',
+        Item = 'yieldsign',
+        Hash = -639994124,
+    },
+}
