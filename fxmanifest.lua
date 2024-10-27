0b9692fda1401803b1720f2feb67ac44ac3005e7
VibrantRP <127861623+VibrantRP@users.noreply.github.com>
2024-10-27 23:35:30 +0000

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Obtaizen & Hyper'
+description 'Sign Robbery Script'
+version '2.0'
+
+client_scripts {
+	'client/*.lua',
+}
+
+server_scripts {
+	'server/*.lua',
+}
+
+shared_scripts {
+	'config.lua',
+	'@ox_lib/init.lua',
+}
+
+lua54 'yes'
